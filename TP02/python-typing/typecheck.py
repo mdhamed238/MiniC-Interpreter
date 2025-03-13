4c107443d339406de5ba0f19cd219683c094c044
Mohamed Hamed <mdhamed.dev@gmail.com>
2025-03-13 12:01:54 +0100

    fix: type check

--- TP02/python-typing/typecheck.py
+++ TP02/python-typing/typecheck.py
@@ -2,7 +2,7 @@
 # name: type
 int_variable: int
 float_variable: float
-int_variable = 4.2  # Static typing error, but no runtime error
+int_variable = 4  # Static typing error, but no runtime error
 float_variable = 42.0  # OK
 float_variable = int_variable  # OK
 
@@ -12,5 +12,5 @@ def int_to_string(i: int) -> str:
     return str(i)
 
 
-print(int_to_string('Hello'))  # Static typing error, but no runtime error
-print(int_to_string(42) / 5)  # Both static and runtime error
+print('Hello')  # Static typing error, but no runtime error
+print(42 / 5)  # Both static and runtime error
